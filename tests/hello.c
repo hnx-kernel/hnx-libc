c5ff991777ce494320d7bb705c339da2b333b6c3
tinc0709 <tinchy@yeah.net>
2025-12-11 07:05:27 +0800

    chore: 从.gitignore中移除tests目录并删除hello.c测试文件
    移除不再需要的测试目录和过时的测试文件hello.c，保持代码库整洁

--- tests/hello.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-void _start() {
-    const char msg[] = "Hello from HNX libc!\n";
-    write(1, msg, sizeof(msg) - 1);
-    
-    // TODO: Replace with exit() once implemented
-    while (1) {}
-}
